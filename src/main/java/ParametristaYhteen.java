71b74ccc9bc178dd90ca5c50c5730410cf2decfb
tm33100 <tuuli.matikainen@gmail.com>
2020-11-08 11:20:21 +0200

    ratkaisu

--- src/main/java/ParametristaYhteen.java
+++ src/main/java/ParametristaYhteen.java
@@ -5,7 +5,16 @@ public class ParametristaYhteen {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        tulostaLuvustaYhteen(5);
+    }
 
+    public static void tulostaLuvustaYhteen(int annettuLuku) {
+        int lkm = 0;
+        int alku = annettuLuku;
+        while (lkm < annettuLuku) {
+            System.out.println(alku - lkm);
+            lkm++;
+        }
     }
 
 }
